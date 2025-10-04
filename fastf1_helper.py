bba20188788de7e6bff26198e8ecdd1e83496334
Apostolis Kopalidis <apostkpl@gmail.com>
2025-10-04 21:40:33 +0300

    Function for downloading season added

--- fastf1_helper.py
+++ fastf1_helper.py
@@ -14,6 +14,58 @@ def get_race(year, race_number):
     except Exception as e:
         raise Exception(f'An error occurred while getting the {year} session: {e}') from e
 
+# Helper function to get a whole season
+def get_season(year):
+    try:
+        # Check if the year given is indeed in the fastf1 database
+        if year < 2018 or year > 2025:
+            raise ValueError(f'Error, Season {year} not available in this database. Available seasons are: 2018-2025')
+        schedule = fastf1.get_event_schedule(year)
+
+        # Create a new dataframe for the current season
+        avg_season_stats = pd.DataFrame()
+        # Repeat the same procedure for every other race in the current session
+        for i in schedule.index:
+            # The first index/row is the test race of the season
+            if i == 0:
+                continue
+            
+            current = get_race(year, i)
+            current.load(laps = True, telemetry = False, weather = False, messages = False)
+
+            # Aggregate important stats into a dataframe
+            avg_current = current.laps.groupby('Driver').agg(
+                avgLapTime = ('LapTime', 'mean'),
+                stdLapTime = ('LapTime', 'std'),
+                lapsCompleted = ('LapNumber', 'max')
+                ).reset_index()
+
+            # Convert Timedelta to seconds
+            avg_current['avgLapTime_s'] = avg_current['avgLapTime'].dt.total_seconds()
+            avg_current['stdLapTime_s'] = avg_current['stdLapTime'].dt.total_seconds()
+            avg_current = avg_current.drop(['avgLapTime', 'stdLapTime'], axis = 1)
+
+            # Merge .laps and .results data
+            avg_current = avg_current.merge(
+                # We have to rename the 'Abbreviation' column, because in our dataframe it's called 'Driver'
+                current.results[['Abbreviation', 'GridPosition', 'Position']].rename(columns = {'Abbreviation': 'Driver'}),
+                how = 'left',
+                on = 'Driver'
+                )
+
+            # Keep track of the race number and the current year
+            avg_current['raceID'] = i
+            avg_current['Year'] = year
+
+            # Add the new rows to the stats dataframe
+            avg_season_stats = pd.concat([avg_season_stats, avg_current], ignore_index = True)
+
+        # Return the whole season stats dataframe
+        return avg_season_stats
+        
+    except Exception as e:
+        raise Exception(f'An error occurred while getting the {year} season: {e}') from e
+
 
 # Helper function that plots the lap times for a driver
 def plot_lap_times(driver_name, laps_df, ax, color = 'steelblue', alpha = 1):
